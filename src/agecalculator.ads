0ca890f9bd1d015082e587f79d2ed764b0c3179c
Tama McGlinn <TGJ.McGlinn@mindef.nl>
2018-09-14 14:08:10 +0200

    Revert "added 13 test cases for DaysInMonth; I don't like this approach to unit testing as it generates 700 lines of skeleton for 13 lines of test code, and has to modify the production code"
    This reverts commit 49d96eb5969a0296c96584b8d7f51834d4fe7182.

--- src/agecalculator.ads
+++ src/agecalculator.ads
@@ -7,20 +7,7 @@ package AgeCalculator is
   type DifferenceDays is range Day_Number'First - Day_Number'Last..Day_Number'Last - Day_Number'First;
   type NumberOfDaysOld is range 0..(366*(Year_Number'Last-Year_Number'First + 1));
 
-  function DaysInMonth(month : in Month_Number; year : in Year_Number) return Day_Number
-    with Test_Case => (Name => "JanuaryHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "FebruaryHas28DaysInCommonYear", Mode => Nominal);
-  pragma Test_Case    (Name => "FebruaryHas29DaysInLeapYear", Mode => Nominal);
-  pragma Test_Case    (Name => "MarchHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "AprilHas30Days", Mode => Nominal);
-  pragma Test_Case    (Name => "MayHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "JuneHas30Days", Mode => Nominal);
-  pragma Test_Case    (Name => "JulyHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "AugustHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "SeptemberHas30Days", Mode => Nominal);
-  pragma Test_Case    (Name => "OctoberHas31Days", Mode => Nominal);
-  pragma Test_Case    (Name => "NovemberHas30Days", Mode => Nominal);
-  pragma Test_Case    (Name => "DecemberHas31Days", Mode => Nominal);
+  function DaysInMonth(month : in Month_Number; year : in Year_Number) return Day_Number;
 
   procedure GetAgeDifference(birth_day : in Day_Number; birth_month : in Month_Number; birth_year : in Year_Number;
                             today_day : in Day_Number; today_month : in Month_Number; today_year : in Year_Number;
--- testharness/agecalculator-test_data-tests-suite.adb
+++ testharness/agecalculator-test_data-tests-suite.adb
@@ -12,95 +12,23 @@ package body AgeCalculator.Test_Data.Tests.Suite is
 
    Result : aliased AUnit.Test_Suites.Test_Suite;
 
-   Case_1_1_Test_DaysInMonth_164afd_e8df0d : aliased Runner_1.Test_Case;
-   Case_2_1_Test_DaysInMonth_164afd_8234fb : aliased Runner_1.Test_Case;
-   Case_3_1_Test_DaysInMonth_164afd_9ce495 : aliased Runner_1.Test_Case;
-   Case_4_1_Test_DaysInMonth_164afd_32932e : aliased Runner_1.Test_Case;
-   Case_5_1_Test_DaysInMonth_164afd_108a74 : aliased Runner_1.Test_Case;
-   Case_6_1_Test_DaysInMonth_164afd_32e647 : aliased Runner_1.Test_Case;
-   Case_7_1_Test_DaysInMonth_164afd_f2c915 : aliased Runner_1.Test_Case;
-   Case_8_1_Test_DaysInMonth_164afd_6a1c72 : aliased Runner_1.Test_Case;
-   Case_9_1_Test_DaysInMonth_164afd_b8163b : aliased Runner_1.Test_Case;
-   Case_10_1_Test_DaysInMonth_164afd_a38e78 : aliased Runner_1.Test_Case;
-   Case_11_1_Test_DaysInMonth_164afd_3d7caf : aliased Runner_1.Test_Case;
-   Case_12_1_Test_DaysInMonth_164afd_e1685b : aliased Runner_1.Test_Case;
-   Case_13_1_Test_DaysInMonth_164afd_cbc9c8 : aliased Runner_1.Test_Case;
-   Case_14_1_Test_GetAgeDifference_062698 : aliased Runner_1.Test_Case;
+   Case_1_1_Test_DaysInMonth_164afd : aliased Runner_1.Test_Case;
+   Case_2_1_Test_GetAgeDifference_062698 : aliased Runner_1.Test_Case;
 
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
    begin
 
       Runner_1.Create
-        (Case_1_1_Test_DaysInMonth_164afd_e8df0d,
-         "agecalculator.ads:11:10:",
-         Test_DaysInMonth_164afd_e8df0d'Access);
+        (Case_1_1_Test_DaysInMonth_164afd,
+         "agecalculator.ads:10:3:",
+         Test_DaysInMonth_164afd'Access);
       Runner_1.Create
-        (Case_2_1_Test_DaysInMonth_164afd_8234fb,
+        (Case_2_1_Test_GetAgeDifference_062698,
          "agecalculator.ads:12:3:",
-         Test_DaysInMonth_164afd_8234fb'Access);
-      Runner_1.Create
-        (Case_3_1_Test_DaysInMonth_164afd_9ce495,
-         "agecalculator.ads:13:3:",
-         Test_DaysInMonth_164afd_9ce495'Access);
-      Runner_1.Create
-        (Case_4_1_Test_DaysInMonth_164afd_32932e,
-         "agecalculator.ads:14:3:",
-         Test_DaysInMonth_164afd_32932e'Access);
-      Runner_1.Create
-        (Case_5_1_Test_DaysInMonth_164afd_108a74,
-         "agecalculator.ads:15:3:",
-         Test_DaysInMonth_164afd_108a74'Access);
-      Runner_1.Create
-        (Case_6_1_Test_DaysInMonth_164afd_32e647,
-         "agecalculator.ads:16:3:",
-         Test_DaysInMonth_164afd_32e647'Access);
-      Runner_1.Create
-        (Case_7_1_Test_DaysInMonth_164afd_f2c915,
-         "agecalculator.ads:17:3:",
-         Test_DaysInMonth_164afd_f2c915'Access);
-      Runner_1.Create
-        (Case_8_1_Test_DaysInMonth_164afd_6a1c72,
-         "agecalculator.ads:18:3:",
-         Test_DaysInMonth_164afd_6a1c72'Access);
-      Runner_1.Create
-        (Case_9_1_Test_DaysInMonth_164afd_b8163b,
-         "agecalculator.ads:19:3:",
-         Test_DaysInMonth_164afd_b8163b'Access);
-      Runner_1.Create
-        (Case_10_1_Test_DaysInMonth_164afd_a38e78,
-         "agecalculator.ads:20:3:",
-         Test_DaysInMonth_164afd_a38e78'Access);
-      Runner_1.Create
-        (Case_11_1_Test_DaysInMonth_164afd_3d7caf,
-         "agecalculator.ads:21:3:",
-         Test_DaysInMonth_164afd_3d7caf'Access);
-      Runner_1.Create
-        (Case_12_1_Test_DaysInMonth_164afd_e1685b,
-         "agecalculator.ads:22:3:",
-         Test_DaysInMonth_164afd_e1685b'Access);
-      Runner_1.Create
-        (Case_13_1_Test_DaysInMonth_164afd_cbc9c8,
-         "agecalculator.ads:23:3:",
-         Test_DaysInMonth_164afd_cbc9c8'Access);
-      Runner_1.Create
-        (Case_14_1_Test_GetAgeDifference_062698,
-         "agecalculator.ads:25:3:",
          Test_GetAgeDifference_062698'Access);
 
-      Result.Add_Test (Case_1_1_Test_DaysInMonth_164afd_e8df0d'Access);
-      Result.Add_Test (Case_2_1_Test_DaysInMonth_164afd_8234fb'Access);
-      Result.Add_Test (Case_3_1_Test_DaysInMonth_164afd_9ce495'Access);
-      Result.Add_Test (Case_4_1_Test_DaysInMonth_164afd_32932e'Access);
-      Result.Add_Test (Case_5_1_Test_DaysInMonth_164afd_108a74'Access);
-      Result.Add_Test (Case_6_1_Test_DaysInMonth_164afd_32e647'Access);
-      Result.Add_Test (Case_7_1_Test_DaysInMonth_164afd_f2c915'Access);
-      Result.Add_Test (Case_8_1_Test_DaysInMonth_164afd_6a1c72'Access);
-      Result.Add_Test (Case_9_1_Test_DaysInMonth_164afd_b8163b'Access);
-      Result.Add_Test (Case_10_1_Test_DaysInMonth_164afd_a38e78'Access);
-      Result.Add_Test (Case_11_1_Test_DaysInMonth_164afd_3d7caf'Access);
-      Result.Add_Test (Case_12_1_Test_DaysInMonth_164afd_e1685b'Access);
-      Result.Add_Test (Case_13_1_Test_DaysInMonth_164afd_cbc9c8'Access);
-      Result.Add_Test (Case_14_1_Test_GetAgeDifference_062698'Access);
+      Result.Add_Test (Case_1_1_Test_DaysInMonth_164afd'Access);
+      Result.Add_Test (Case_2_1_Test_GetAgeDifference_062698'Access);
 
       return Result'Access;
 
